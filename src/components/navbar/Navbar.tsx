9f0bfc756d644ca8adfadbb718164dd2f7d64d15
Jonas-Bostoen <79192203+Jonas-Bostoen@users.noreply.github.com>
2022-08-17 09:30:33 +0000

    small ui updates

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -6,11 +6,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCloudRain } from '@fortawesome/free-solid-svg-icons';
 
 export default function Navbar() {
-  const { changeLang, language, system, changeSystem } = useContext(WeatherContext);
-
-  const handleLanguage = useCallback((props: any) => {
-    changeLang(props)
-  }, [changeLang]);
+  const { system, changeSystem } = useContext(WeatherContext);
 
   const handleSystem = useCallback((props: any) => {
     changeSystem(props)
@@ -22,17 +18,15 @@ export default function Navbar() {
         <FontAwesomeIcon icon={faCloudRain} />
       </div>
 
-      <Link to='/' >Today</Link>
-      <Link to='/forecast' >Forecast</Link>
+      <div className="buttons">
+        <Link to='/' >Today</Link>
+        <Link to='/forecast' >Forecast</Link>
+      </div>
 
       <div className="options">
         <select defaultValue={system}>
           <option value="metric" onClick={() => handleSystem('metric')}>Metric</option>
           <option value="imperial" onClick={() => handleSystem('imperial')}>Imperial</option>
-        </select>
-        <select defaultValue={language}>
-          <option value="" onClick={() => handleLanguage('')}> eng</option>
-          <option value="nl" onClick={() => handleLanguage('nl')}> nl</option>
         </select>
       </div>
     </nav>
--- src/contexts/WeatherProvider.tsx
+++ src/contexts/WeatherProvider.tsx
@@ -12,28 +12,17 @@ export default function WeatherProvider(props: { children: any }) {
   const [error, setError] = useState('');
   // Options
   const [system, setSystem] = useState(localStorage.getItem('system') ? localStorage.getItem('system') : 'metric');
-  const [language, setLanguage] = useState(localStorage.getItem('lang') ? localStorage.getItem('lang') : 'nl');
 
   const getWeatherInfo = useCallback(async () => {
     try {
       setLoading(true);
       setError('');
-      if (language === '') {
-        setData((await weatherAPI.getCurrentWeatherViaIp()).data);
-      } else {
-        setData((await weatherAPI.getCurrentWeatherViaIpDutch()).data);
-      }
+      setData((await weatherAPI.getCurrentWeatherViaIp()).data);
     } catch (error: any) {
       setError(error);
     } finally {
       setLoading(false);
     }
-  }, [language]);
-
-  // Changes the language of the app
-  const changeLang = useCallback((value: string) => {
-    setLanguage(value);
-    localStorage.setItem('lang', value);
   }, []);
 
   // Changes the usage of the metric or imperial system
@@ -44,8 +33,8 @@ export default function WeatherProvider(props: { children: any }) {
 
   // Provider values
   const values = useMemo(() => ({
-    data, loading, error, getWeatherInfo, language, changeLang, system, changeSystem
-  }), [changeLang, changeSystem, data, error, getWeatherInfo, language, loading, system]);
+    data, loading, error, getWeatherInfo, system, changeSystem
+  }), [changeSystem, data, error, getWeatherInfo, loading, system]);
 
   return (
     <WeatherContext.Provider value={values}>
